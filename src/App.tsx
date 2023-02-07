2e26e8f0837afd8b554f9170db55db440b7e9f11
teikim98 <teikim98@gmail.com>
2023-02-07 17:52:33 +0900

    jsx tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-
+import "./input.css"
 
 function App() {
+
   return (
     <>
-      <h1>hello</h1>
+      <div className="min-h-screen bg-gray-500">
+        <h2 className="text-center text-4xl font-bold text-blue-900">TailwindCSS + React</h2>
+      </div>
     </>
   );
 }
