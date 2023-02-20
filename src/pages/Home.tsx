29d730db71f32d3508d085b2ab146308fcf38237
teikim98 <teikim98@gmail.com>
2023-02-20 15:25:15 +0900

    use query connect

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,101 +1,27 @@
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
 import { useQuery } from "react-query";
-import { fetchPokemons } from '../api/api';
-import { useState } from 'react';
+import { PokemonsResponseResult } from "../@types/api";
+import { pokemonApi } from '../api/'
+import PokemonCards from "../Components/PokemonCards";
 
-export interface IPokemon {
-  id: number,
-  name: string,
-  sprites: {
-    back_default: string
-    front_default: string
-  }
-  types: [
-    {
-      slot: number,
-      type: {
-        name: string
-        url: string
-      }
-    }
-  ]
-}
+const Home: React.FC<{}> = () => {
 
-const Container = styled.div`
-  padding: 0px 20px;
-  max-width: 480px;
-  margin: 0 auto;
-`;
-const Header = styled.header`
-  height: 15vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-const CoinsList = styled.ul``;
-const Coin = styled.li`
-  background-color: white;
-  color: #afafaf;
-  border-radius: 15px;
-  margin-bottom: 10px;
-  a {
-    display: flex;
-    align-items: center;
-    padding: 20px;
-    transition: color 0.2s ease-in;
-  }
-  &:hover {
-    a {
-      color: #7878ff;
-    }
-  }
-`;
-const Title = styled.h1`
-  font-size: 48px;
-  color: #7878ff;
-`;
-const Loader = styled.span`
-  text-align: center;
-  display: block;
-`;
-const Img = styled.img`
-  width: 35px;
-  height: 35px;
-  margin-right: 10px;
-`;
+  const pokemons = useQuery("pokemons", () => {
+    return pokemonApi.getAllPokemons();
+  })
 
-function Home() {
-
-  const [poke, setPoke] = useState<IPokemon[]>();
-
-  const getPokemons = () => {
-
-    const pokemons = [];
-    for (var i = 1; i <= 10; i++) {
-      pokemons.push(fetchPokemons(i));
-    }
-    setPoke(pokemons);
+  if (pokemons.isLoading) {
+    return <div>loading</div>;
   }
 
   return (
-    <Container>
-      <Header>
-        <Title>Poke</Title>
-      </Header>
-      {isLoading ? (
-        <Loader>Loading...</Loader>
-      ) : (
-        <CoinsList>
-          {data?.results.map((poke) => (
-            <div key={poke.name}>
-              <h1>{poke.name}</h1>
-            </div>
-          ))}
-        </CoinsList>
-      )}
-    </Container>
-  );
+    <div>
+      <h1>Home</h1>
+      {pokemons.data?.data.results.map((pokemon: PokemonsResponseResult) => (
+        <PokemonCards name={pokemon.name} />
+      ))}
+
+    </div>
+  )
 }
 
 export default Home;
